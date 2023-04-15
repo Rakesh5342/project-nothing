9c4048f33485a47fbcb3e0a7575b0aa8e2a7981a
Rakesh <ramborakesh661@gmail.com>
2023-04-15 22:04:36 +0530

    Commiting code and Example

---
+++ add.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+ int main()
+{
+	int number;
+	
+	//int add=a+b;
+	cout<<"Enter an integer: ";
+	cin >> number;
+	cout<< "you entered :"<< number;
+	//cout<<"\nHello World";
+}
+
+	
